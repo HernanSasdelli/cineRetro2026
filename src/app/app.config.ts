507996b55b8e0630eb7d49911091d1b1678c79cf
HernanSasdelli <her9192copia@gmail.com>
2026-09-24 17:08:49 -0300

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
 import localeEsAr from '@angular/common/locales/es-AR';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localeEsAr);
 
@@ -10,6 +11,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
-    { provide: LOCALE_ID, useValue: 'es-AR' }, // fechas y $ en formato argentino
+    { provide: LOCALE_ID, useValue: 'es-AR' }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), // fechas y $ en formato argentino
   ],
 };
